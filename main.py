c5490844a89a5177572f826dd484171444a62563
BuildTools <unconfigured@null.spigotmc.org>
2023-06-06 01:00:07 +0300

    feat: OS File queue download functionality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import src.video_downloader.twitch as twitch
 import src.video_downloader.youtube as youtube
+import src.video_downloader.os_file_queue as FileQueue
 from src.video_downloader.storage_manager import StorageManager
 from src.twitch_api import api_client
 from concurrent.futures import ThreadPoolExecutor
-from typing import Set
+from typing import Set, List
 import config
 import sys
 import os
@@ -19,18 +20,6 @@ SENTINEL = "STOP_WORKER"
 stop_workers = threading.Event()
 
 
-def process_queue(q: queue.Queue):
-    while True:
-        try:
-            url = q.get(block=True)
-            if url == SENTINEL:
-                break
-            process_url(url)
-            q.task_done()
-        except Exception as e:
-            traceback.print_exc()
-
-
 def process_url(url: str):
     if "twitch.tv" in url:
         print("Twitch url detected.")
@@ -44,6 +33,7 @@ def process_url(url: str):
         if youtube.is_youtube_playlist(url):
             print(f"Playlist detected: {url}")
             videos = youtube.get_playlist_video_urls(url)
+            print(f"Got {len(videos)} videos.")
             download_dir = youtube.get_playlist_download_directory(
                 config.STREAM_DOWNLOADS, url
             )
@@ -106,7 +96,14 @@ def download_audio(
         traceback.print_exc()
 
 
-def main():
+class DownloaderConfig:
+    def __init__(self, urls: List[str], num_worker_threads: int, file_queue_mode: bool):
+        self.urls: List[str] = urls
+        self.num_worker_threads: int = num_worker_threads
+        self.file_queue_mode: bool = file_queue_mode
+
+
+def parse_args() -> DownloaderConfig:
     parser = argparse.ArgumentParser(
         description="Download and convert videos from different platforms"
     )
@@ -117,39 +114,103 @@ def main():
         default=4,
         help="Number of worker threads (default: 4)",
     )
+    parser.add_argument(
+        "--file-queue-mode",
+        type=bool,
+        default=False,
+        help="Should the program listen to the file queue.",
+    )
     args = parser.parse_args()
+    return DownloaderConfig(args.urls, args.num_worker_threads, args.file_queue_mode)
 
-    urls: Set[str] = set(args.urls)
-    q = queue.Queue()
-    num_worker_threads = args.num_worker_threads
+
+def start_worker_threads(
+    num_worker_threads: int, q: queue.Queue, file_queue: FileQueue.OSFileQueue
+) -> List[threading.Thread]:
     threads = []
-
-    # total_songs, total_duration = utils.count_songs_and_duration(
-    #     config.STREAM_DOWNLOADS
-    # )
-    # print(f"Total songs: {total_songs}")
-    # print(f"Total duration: {utils.format_duration(total_duration)}")
-
-    for url in urls:
-        q.put(url)
-
     for _ in range(num_worker_threads):
-        t = threading.Thread(target=process_queue, args=(q,))
+        t = threading.Thread(target=process_queue, args=(q, file_queue))
         t.setDaemon(True)
         t.start()
         threads.append(t)
+    return threads
 
+
+def process_queue(q: queue.Queue, file_queue: FileQueue.OSFileQueue):
+    while True:
+        try:
+            url = q.get(block=True)
+            if url == SENTINEL:
+                break
+            file_queue.remove_from_input(url)
+            file_queue.add_to_ongoing(url)
+            process_url(url)
+            file_queue.remove_from_ongoing(url)
+            file_queue.add_to_finished(url)
+            q.task_done()
+        except Exception as e:
+            traceback.print_exc()
+
+
+def handle_user_input(q: queue.Queue):
     while True:
         try:
             url = input("Enter URL: ")
             q.put(url)
-        except KeyboardInterrupt:
+        except (EOFError, KeyboardInterrupt):
             print("Exiting program...")
             stop_workers.set()
             break
 
+
+def load_ongoing(q: queue.Queue, file_queue: FileQueue.OSFileQueue):
+    urls = file_queue.read_from_ongoing()
+    for url in urls:
+        url = url.strip()
+        q.put(url)
+
+
+def handle_file_input(q: queue.Queue, file_queue: FileQueue.OSFileQueue):
+    queued_urls = set()  # keep track of urls that have been added to the queue
+    while not stop_workers.is_set():
+        urls = file_queue.read_from_input()
+        print(f"{len(urls)} urls found in input file.")
+        for url in urls:
+            url = url.strip()
+            if url not in queued_urls:
+                q.put(url)
+                queued_urls.add(url)  # add the url to the set of queued urls
+        time.sleep(2)  # sleep for 2 seconds
+
+
+def main():
+    args = parse_args()
+    file_queue = FileQueue.OSFileQueue(config.STREAM_DOWNLOADS)
+    q = queue.Queue()
+
+    for url in set(args.urls):
+        file_queue.add_to_input(url)
+
+    load_ongoing(q, file_queue)
+
+    threads = start_worker_threads(args.num_worker_threads, q, file_queue)
+
+    user_input_thread = threading.Thread(target=handle_user_input, args=(q,))
+    user_input_thread.setDaemon(True)
+    user_input_thread.start()
+
+    file_input_thread = threading.Thread(target=handle_file_input, args=(q, file_queue))
+    file_input_thread.setDaemon(True)
+    file_input_thread.start()
+    try:
+        user_input_thread.join()
+        file_input_thread.join()
+    except KeyboardInterrupt:
+        # Exiting program
+        pass
+
     # Add sentinel values to the queue to signal worker threads to exit
-    for _ in range(num_worker_threads):
+    for _ in range(args.num_worker_threads):
         q.put(SENTINEL)
 
     # Wait for all threads to finish
---
+++ src/video_downloader/os_file_queue.py
@@ -0,0 +1,74 @@
+import threading
+import os.path as path
+import os
+from typing import List
+
+
+class OSFileQueue:
+    def __init__(self, library_location: str):
+        self.file_lock = threading.Lock()
+        self.work_dir = path.join(library_location, ".work_stats")
+        os.makedirs(self.work_dir, exist_ok=True)
+
+        self.input_file = path.join(self.work_dir, "input.txt")
+        self.ongoing_file = path.join(self.work_dir, "ongoing.txt")
+        self.finished_file = path.join(self.work_dir, "finished.txt")
+        # Create the files if they don't exist
+        for file in [self.input_file, self.ongoing_file, self.finished_file]:
+            if not path.isfile(file):
+                open(file, "w").close()
+
+    def add_to_input(self, url: str):
+        url = url.strip()
+        with self.file_lock:
+            with open(self.input_file, "r") as file:
+                lines = file.readlines()
+                lines = [line.strip() for line in lines]
+            if url not in lines:
+                with open(self.input_file, "a") as file:
+                    file.write(url + "\n")
+
+    def read_from_input(self):
+        with self.file_lock:
+            with open(self.input_file, "r") as file:
+                lines = file.readlines()
+        return lines
+
+    def read_from_ongoing(self) -> List[str]:
+        with self.file_lock:
+            with open(self.ongoing_file, "r") as file:
+                lines = file.readlines()
+        return lines
+
+    def add_to_ongoing(self, url: str):
+        url = url.strip()
+        with self.file_lock:
+            with open(self.ongoing_file, "r") as file:
+                lines = file.readlines()
+                lines = [line.strip() for line in lines]
+            if url not in lines:
+                with open(self.ongoing_file, "a") as file:
+                    file.write(url + "\n")
+
+    def remove_from_ongoing(self, url):
+        with self.file_lock:
+            with open(self.ongoing_file, "r") as file:
+                lines = file.readlines()
+            with open(self.ongoing_file, "w") as file:
+                for line in lines:
+                    if line.strip("\n") != url:
+                        file.write(line)
+
+    def add_to_finished(self, url):
+        with self.file_lock:
+            with open(self.finished_file, "a") as file:
+                file.write(url + "\n")
+
+    def remove_from_input(self, url):
+        with self.file_lock:
+            with open(self.input_file, "r") as file:
+                lines = file.readlines()
+            with open(self.input_file, "w") as file:
+                for line in lines:
+                    if line.strip("\n") != url:
+                        file.write(line)
