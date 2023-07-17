06f4a90bfda1d79811a5efadd64e876fb5c480d2
BuildTools <unconfigured@null.spigotmc.org>
2023-07-17 23:08:01 +0300

    refactor: OS_FILE_QUEUE embarrassing refactorization and code improvement.

--- src/video_downloader/os_file_queue.py
+++ src/video_downloader/os_file_queue.py
@@ -4,19 +4,51 @@ import os
 from typing import List
 
 
+class QueueFile:
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+        self.file_lock = threading.Lock()
+        if not path.isfile(filepath):
+            open(filepath, "w").close()
+
+    def read(self) -> List[str]:
+        with self.file_lock:
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+        return lines
+
+    def add(self, url: str):
+        url = url.strip()
+        with self.file_lock:
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+                lines = [line.strip() for line in lines]
+            if url not in lines:
+                with open(self.filepath, "a") as file:
+                    file.write(url + "\n")
+
+    def remove(self, url):
+        with self.file_lock:
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+            with open(self.filepath, "w") as file:
+                for line in lines:
+                    if line.strip("\n") != url:
+                        file.write(line)
+
+
 class OSFileQueue:
     def __init__(self, library_location: str, file_type: str):
-        self.file_lock = threading.Lock()
         self.work_dir = path.join(library_location, ".work_stats")
         os.makedirs(self.work_dir, exist_ok=True)
 
-        self.input_file = path.join(self.work_dir, f"{file_type}_input.txt")
-        self.ongoing_file = path.join(self.work_dir, f"{file_type}_ongoing.txt")
-        self.finished_file = path.join(self.work_dir, f"{file_type}_finished.txt")
-        # Create the files if they don't exist
-        for file in [self.input_file, self.ongoing_file, self.finished_file]:
-            if not path.isfile(file):
-                open(file, "w").close()
+        self.input_file = QueueFile(path.join(self.work_dir, f"{file_type}_input.txt"))
+        self.ongoing_file = QueueFile(
+            path.join(self.work_dir, f"{file_type}_ongoing.txt")
+        )
+        self.finished_file = QueueFile(
+            path.join(self.work_dir, f"{file_type}_finished.txt")
+        )
 
     def format_input_file(self):
         def gather_unique_urls(lines: List[str]):
@@ -28,70 +60,9 @@ class OSFileQueue:
                         unique_urls.append(url)
             return unique_urls
 
-        with self.file_lock:
-            with open(self.input_file, "r") as input_file:
-                lines = input_file.readlines()
-            unique_urls = gather_unique_urls(lines)
-            with open(self.input_file, "w") as file:
+        lines = self.input_file.read()
+        unique_urls = gather_unique_urls(lines)
+        with self.input_file.file_lock:
+            with open(self.input_file.filepath, "w") as file:
                 for url in unique_urls:
                     file.write(url + "\n")
-
-    def add_to_input(self, url: str):
-        url = url.strip()
-        with self.file_lock:
-            with open(self.input_file, "r") as file:
-                lines = file.readlines()
-                lines = [line.strip() for line in lines]
-            if url not in lines:
-                with open(self.input_file, "a") as file:
-                    file.write(url + "\n")
-
-    def read_from_input(self):
-        with self.file_lock:
-            with open(self.input_file, "r") as file:
-                lines = file.readlines()
-        return lines
-
-    def read_from_ongoing(self) -> List[str]:
-        with self.file_lock:
-            with open(self.ongoing_file, "r") as file:
-                lines = file.readlines()
-        return lines
-
-    def add_to_ongoing(self, url: str):
-        url = url.strip()
-        with self.file_lock:
-            with open(self.ongoing_file, "r") as file:
-                lines = file.readlines()
-                lines = [line.strip() for line in lines]
-            if url not in lines:
-                with open(self.ongoing_file, "a") as file:
-                    file.write(url + "\n")
-
-    def remove_from_ongoing(self, url):
-        with self.file_lock:
-            with open(self.ongoing_file, "r") as file:
-                lines = file.readlines()
-            with open(self.ongoing_file, "w") as file:
-                for line in lines:
-                    if line.strip("\n") != url:
-                        file.write(line)
-
-    def add_to_finished(self, url: str):
-        url = url.strip()
-        with self.file_lock:
-            with open(self.finished_file, "r") as file:
-                lines = file.readlines()
-                lines = [line.strip() for line in lines]
-            if url not in lines:
-                with open(self.finished_file, "a") as file:
-                    file.write(url + "\n")
-
-    def remove_from_input(self, url):
-        with self.file_lock:
-            with open(self.input_file, "r") as file:
-                lines = file.readlines()
-            with open(self.input_file, "w") as file:
-                for line in lines:
-                    if line.strip("\n") != url:
-                        file.write(line)
