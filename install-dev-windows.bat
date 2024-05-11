8cf2c0d365f5a584d9f25d8342fe809bb094c54d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 16:42:26 +0300

    update install script

--- install-dev-windows.bat
+++ install-dev-windows.bat
@@ -1,9 +1,7 @@
 @echo off
 
 REM Check if virtual environment exists
-if exist venv (
-    call venv\scripts\activate
-) else (
+if not exist venv (
     REM Try to create virtual environment with 'py'
     py -m venv venv
     if errorlevel 1 (
@@ -14,16 +12,13 @@ if exist venv (
             exit /b
         )
     )
-    call venv\scripts\activate
 )
 
-REM Try to install requirements with 'py'
-py -m pip install -r requirements.txt
+call venv\Scripts\activate
+
+REM Use the activated environment's pip
+pip install -r requirements.txt
 if errorlevel 1 (
-    REM If 'py' fails, try 'python3'
-    python3 -m pip install -r requirements.txt
-    if errorlevel 1 (
-        echo Could not install requirements.
-        exit /b
-    )
+    echo Could not install requirements.
+    exit /b
 )
