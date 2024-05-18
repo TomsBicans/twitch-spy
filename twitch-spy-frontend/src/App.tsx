197a1cfedfe10e92c8d2a30868c2d14ad39af541
Toms Bicāns <tomsbicans@gmail.com>
2024-05-18 18:50:07 +0300

    remove unused imports

--- twitch-spy-frontend/src/App.tsx
+++ twitch-spy-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import io from "socket.io-client";
 import "./App.css";
 import URLInput from "./components/URLInput";
