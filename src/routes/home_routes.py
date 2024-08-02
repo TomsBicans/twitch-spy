dcf6ca746875c07d092baebcae0b63273c808130
Toms Bicāns <tomsbicans@gmail.com>
2024-08-03 00:09:06 +0300

    fix form submit endpoint - now working

--- src/routes/home_routes.py
+++ src/routes/home_routes.py
@@ -17,42 +17,38 @@ from src.system_logger import logger
 home_routes = Blueprint("home_routes", __name__)
 
 
-@home_routes.route("/test")
-def home():
-    return render_template("home.html")
-
-
-@home_routes.route("/", methods=["GET", "POST"])
-def get_queue():
-    logger.debug("Handling root route.")
-    # Get the MyApp instance from the Flask app's configuration
+@home_routes.route("/form_submit", methods=["POST"])
+def form_submit():
+    logger.debug("Handling form submit.")
     my_app: app.Application = flask.current_app.config[util.MagicStrings.APP]
 
-    if flask.request.method == "POST":
-        logger.debug("POST method detected")
-        urls = flask.request.form.get("urls")
-        if urls:
-            urls_list = [url.strip() for url in urls.split("\n")]
-            logger.debug(f"URLs list: {urls_list}")
-            user_input = sm.StorageManager(config.STREAM_DOWNLOADS)
-            for url in urls_list:
-                user_input.mark_successful_download(url)
-            jobs = ph.Atomizer.atomize_urls(
-                urls_list,
-                const.CONTENT_MODE.AUDIO,
-                config.AUDIO_LIBRARY,
+    logger.debug("POST method detected")
+    data = flask.request.json
+    urls = data.get("urls")
+    logger.debug(f"URLs: {urls}")
+    if urls:
+        urls_list = [url.strip() for url in urls.split("\n")]
+        logger.debug(f"URLs list: {urls_list}")
+        user_input = sm.StorageManager(config.STREAM_DOWNLOADS)
+        for url in urls_list:
+            user_input.mark_successful_download(url)
+        jobs = ph.Atomizer.atomize_urls(
+            urls_list,
+            const.CONTENT_MODE.AUDIO,
+            config.AUDIO_LIBRARY,
+        )
+        logger.debug(f"Jobs created: {jobs}")
+        logger.debug(f"Number of jobs: {len(jobs)}")
+        for job in jobs:
+            my_app.job_manager.add_job(job)
+            my_app.event_dispatcher.dispatch_event(Events.JOB_CREATED.value, job)
+            my_app.event_dispatcher.dispatch_event(
+                Events.STATISTICS_UPDATE.value, my_app.job_manager.stats
             )
-            logger.debug(f"Jobs created: {jobs}")
-            logger.debug(f"Number of jobs: {len(jobs)}")
-            for job in jobs:
-                my_app.job_manager.add_job(job)
-                my_app.event_dispatcher.dispatch_event(Events.JOB_CREATED.value, job)
-                my_app.event_dispatcher.dispatch_event(
-                    Events.STATISTICS_UPDATE.value, my_app.job_manager.stats
-                )
 
-            return jsonify({"success": True})
-    return render_template("home.html")
+        return jsonify({"success": True})
+
+    return jsonify({"success": False})
 
 
 @socketio.on("request_initial_data")
